8941dbb70a5739c0bde2953e9ffd4da7a1d7d769
Techtix0 <techtix0@proton.me>
2025-11-08 20:58:18 +0100

    Add: initial grid display

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    println!("┌─┬─┬─┬─┬─┐");
+    println!("├─┼─┼─┼─┼─┤");
+    println!("├─┼─┼─┼─┼─┤");
+    println!("├─┼─┼─┼─┼─┤");
+    println!("├─┼─┼─┼─┼─┤");
+    println!("└─┴─┴─┴─┴─┘");
 }
