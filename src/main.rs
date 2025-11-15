cf99a38dc5377e56798de3930f6da2b38f1e70c9
Techtix0 <techtix0@proton.me>
2025-11-15 23:20:19 +0100

    WIP: alive cells

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,33 @@
 use crossterm_cursor::{self, TerminalCursor};
+use std::time::{Duration, SystemTime};
 
 fn main() {
     let cursor = TerminalCursor::new();
     TerminalCursor::hide(&cursor).expect("error while trying to hide cursor");
 
     let grid = Grid {
-        length: 10,
-        height: 10,
+        length: 3,
+        height: 3,
+        alive_cells: vec![(0, 0), (0, 2), (1, 1)],
     };
 
+    let start = SystemTime::now();
+
     println!("{}", grid.generate_grid());
+
+    let end = SystemTime::now();
+    let duration = end.duration_since(start).expect("error");
+
+    println!("{duration:?}");
 
     TerminalCursor::show(&cursor).expect("error while trying to show cursor");
 }
 
+#[derive(Default)]
 struct Grid {
     length: u32,
     height: u32,
+    alive_cells: Vec<(u32, u32)>,
 }
 
 impl Grid {
@@ -32,10 +43,11 @@ impl Grid {
         const BOTTOM_SPLIT: char = '┴';
         const LEFT_SPLIT: char = '├';
         const RIGHT_SPLIT: char = '┤';
-        const CELL_INSIDE: &'static str = "   ";
-    
+        const DEAD_CELL: &'static str = "   ";
+        const ALIVE_CELL: &'static str = "▐█▌";
+
         let mut result: String = String::from("");
-    
+
         // Generate top row of grid
         result.push(TOP_LEFT);
         for _ in 0..self.length - 1 {
@@ -44,18 +56,25 @@ impl Grid {
         }
         result.push_str(HORIZONTAL_LINE);
         result.push(TOP_RIGHT);
-    
+
         // Generate middle rows of grid
         for i in 0..(self.height * 2) - 1 {
+            result.push_str("\n");
             if i % 2 == 0 {
-                result.push_str("\n");
-                for _ in 0..self.length {
+                for j in 0..self.length {
                     result.push(VERTICAL_LINE);
-                    result.push_str(CELL_INSIDE);
+                    match self.alive_cells.contains(&(j, i)) {
+                        true => {
+                            result.push_str(ALIVE_CELL);
+                        }
+
+                        false => {
+                            result.push_str(DEAD_CELL);
+                        }
+                    }
                 }
                 result.push(VERTICAL_LINE);
             } else {
-                result.push_str("\n");
                 result.push(LEFT_SPLIT);
                 for _ in 0..self.length - 1 {
                     result.push_str(HORIZONTAL_LINE);
@@ -65,7 +84,7 @@ impl Grid {
                 result.push(RIGHT_SPLIT);
             }
         }
-    
+
         // Generate bottom row of grid
         result.push_str("\n");
         result.push(BOTTOM_LEFT);
@@ -75,8 +94,7 @@ impl Grid {
         }
         result.push_str(HORIZONTAL_LINE);
         result.push(BOTTOM_RIGHT);
-    
+
         result
     }
 }
-
